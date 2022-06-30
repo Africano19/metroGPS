dbc7df9a8ed72705d14c67e84025e1fb02aa54c2
Africano19 <ruben.africano.p@gmail.com>
2022-06-30 21:37:30 +0100

    .

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
 <link rel="stylesheet" href="assets/css/style.css">
-<script src="assets/js/map.js"></script> 
 </head>
 <body>
 
@@ -64,8 +63,7 @@ function onClick(element) {
   var captionText = document.getElementById("caption");
   captionText.innerHTML = element.alt;
 }
-
-<script src="myScript.js"></script> 
+<script src="assets/js/map.js"></script> 
 </script>
 
 </body>

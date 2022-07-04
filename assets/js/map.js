2cae868b2a69486ca1704fe35b5d8991a5ac97c1
Africano19 <81010248+Africano19@users.noreply.github.com>
2022-07-04 21:32:11 +0100

    .

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,10 +1,11 @@
-mapboxgl.accessToken = 'sk.eyJ1IjoiYWZyaWNhbm8xOSIsImEiOiJjbDU3NnQ4dXQwY3oxM2VxbmdxeWkyMnhnIn0.sbBm3Zv3ozbLKt_6ep4hig';
-
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWZyaWNhbm8xOSIsImEiOiJjbDUxaGY1YXIwNXFoM2NxcG1najZmZGpvIn0._lqJtlkSKz5RM6GHyIGkbg';
 const map = new mapboxgl.Map({
-  container: 'map', // HTML container ID
-  style: 'mapbox://styles/mapbox/streets-v11', // style URL
-  center: [-21.9270884, 64.1436456], // starting position [lng, lat]
-  zoom: 13 // starting zoom
-  
+container: 'map', // container ID
+style: 'mapbox://styles/mapbox/streets-v11', // style URL
+center: [-74.5, 40], // starting position [lng, lat]
+zoom: 9, // starting zoom
+projection: 'globe' // display the map as a 3D globe
 });
-
+map.on('style.load', () => {
+map.setFog({}); // Set the default atmosphere style
+});

09cfdf807a8effd3faedb35dacc9524eb848e03b
Africano19 <81010248+Africano19@users.noreply.github.com>
2022-07-04 21:30:22 +0100

    .

--- index.php
+++ index.php
@@ -6,9 +6,8 @@
   <title>Switch to Mapbox</title>
   <meta name='viewport' content='width=device-width, initial-scale=1' />
   <!-- Import Mapbox GL JS  -->
-  <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.js'></script>
-  <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.css' rel='stylesheet' />
-
+  <script src='https://api.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.js'></script>
+<link href='https://api.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.css' rel='stylesheet' />
   <style>
     body {
       margin: 0;
@@ -26,14 +25,20 @@
 
 <body>
   <!-- Create a container for the map. -->
-  <div id='map'></div>
-
-  <script>
-    // Add your Mapbox access token.
-    mapboxgl.accessToken = 'pk.eyJ1IjoiYWZyaWNhbm8xOSIsImEiOiJjbDUxaGY1YXIwNXFoM2NxcG1najZmZGpvIn0._lqJtlkSKz5RM6GHyIGkbg';
-
-    // Code from the next step will go here.
-  </script>
+  <div id='map' style='width: 400px; height: 300px;'></div>
+<script>
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWZyaWNhbm8xOSIsImEiOiJjbDUxaGY1YXIwNXFoM2NxcG1najZmZGpvIn0._lqJtlkSKz5RM6GHyIGkbg';
+const map = new mapboxgl.Map({
+container: 'map', // container ID
+style: 'mapbox://styles/mapbox/streets-v11', // style URL
+center: [-74.5, 40], // starting position [lng, lat]
+zoom: 9, // starting zoom
+projection: 'globe' // display the map as a 3D globe
+});
+map.on('style.load', () => {
+map.setFog({}); // Set the default atmosphere style
+});
+</script>
 </body>
 
 </html>

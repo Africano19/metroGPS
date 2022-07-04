a922d080e59af3f3245e07e4d40d815fc5a63a5d
Africano19 <81010248+Africano19@users.noreply.github.com>
2022-07-04 21:51:57 +0100

    .

--- assets/js/map.js
+++ assets/js/map.js
@@ -2,10 +2,25 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYWZyaWNhbm8xOSIsImEiOiJjbDUxaGY1YXIwNXFoM2Nxc
 const map = new mapboxgl.Map({
 container: 'mapBox', // container ID
 style: 'mapbox://styles/mapbox/streets-v11', // style URL
-center: [-74.5, 40], // starting position [lng, lat]
-zoom: 9, // starting zoom
+center: [38.736946, -9.142685],
+zoom: 13,
 projection: 'globe' // display the map as a 3D globe
 });
 map.on('style.load', () => {
 map.setFog({}); // Set the default atmosphere style
 });
+getLocation();
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(showPosition);
+  } else {
+    map.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+	let mark = L.marker([position.coords.latitude, position.coords.longitude]).bindPopup("My Location").addTo(map);
+	myLocation = position.coords;
+	mark._icon.classList.add("myLoc");
+}

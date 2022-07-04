a5391b0228d32ba2b17f59458f70f6f02f4d6176
Africano19 <ruben.africano.p@gmail.com>
2022-07-04 19:22:18 +0100

    .

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ body, h1,h2,h3,h4,h5,h6 {font-family: "Montserrat", sans-serif}
     <h2 class="w3-text-light-grey">Estações</h2>
     <hr style="width:200px" class="w3-opacity">
     
-    <div id="googleMap" style="width:100%;height:550px;"></div>
+    <div id="googleMap" style="width:100%;height:600px;"></div>
   
   <!-- End Contact Section -->
   </div>

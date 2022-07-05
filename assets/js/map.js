caef8a08957d920cc5e204e088b8b2d4e44a6ab2
Africano19 <ruben.africano.p@gmail.com>
2022-07-05 22:44:20 +0100

    .

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,3 +1,7 @@
+src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"
+
 var myLocation;
 var homeLocation;
 let map, infoWindow;

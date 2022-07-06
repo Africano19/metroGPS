6201ec458b23759537fefb310aa61caecd780fb6
Africano19 <ruben.africano.p@gmail.com>
2022-07-06 19:55:03 +0100

    .

--- assets/js/map.js
+++ assets/js/map.js
@@ -123,6 +123,28 @@ function allStations(){
 	});
 }
 
+
+function calculateRouteFromAtoB() {
+  var router = google.getRoutingService(null, 8),
+      routeRequestParams = {
+        routingMode: 'fast',
+        transportMode: 'car',
+        origin: '52.5160,13.3779', // Brandenburg Gate
+        destination: '52.5206,13.3862', // Friedrichstraße Railway Station
+        return: 'polyline,turnByTurnActions,actions,instructions,travelSummary'
+      };
+
+  router.calculateRoute(
+    routeRequestParams,
+    onSuccess,
+    onError
+  );
+}
+
+
+
+calculateRouteFromAtoB();
+
 // Linha Verde
 $(document).ready(function() {
   $("#greenLine").click(function(e) {

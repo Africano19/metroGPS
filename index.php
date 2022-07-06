57f405d7551c61cf514b116880a2ce49fcf0320e
Africano19 <ruben.africano.p@gmail.com>
2022-07-06 22:49:48 +0100

    .

--- index.php
+++ index.php
@@ -68,8 +68,8 @@ body, h1,h2,h3,h4,h5,h6 {font-family: "Montserrat", sans-serif}
     <h2 class="w3-text-light-grey">Estações</h2>
     <hr style="width:200px" class="w3-opacity">
     <div class="w3-bar">
-      <button class="w3-bar-item w3-button button5" id="allLines" style="width:33.3%;">Todas as Linhas</button>
       <button class="w3-bar-item w3-button button2" id="yellowLine" value="yellowLine" style="width:33.3%;">Linha Amarela</button>
+      <button class="w3-bar-item w3-button button5" id="allLines" style="width:33.3%;">Todas as Linhas</button>
       <button class="w3-bar-item w3-button button3" id="blueLine" value="blueLine" style="width:33.3%; ">Linha Azul</button>
       <button class="w3-bar-item w3-button button4" id="redLine" value="redLine" value="redLine" style="width:50%;">Linha Vermelha</button>
       <button class="w3-bar-item w3-button button" id="greenLine" value="greenLine" style="width:50%; ">Linha Verde</button>  
